6fd39b15fb7c1a5e747b816092a8b029cc5740f8
Florian Jung <florian.a.jung@web.de>
2013-05-19 18:37:11 +0200

    GPLized

--- synth/readwave.h
+++ synth/readwave.h
@@ -1,3 +1,23 @@
+/*
+    Copyright (C) 2010-2012 Florian Jung
+     
+    This file is part of flo's FM synth.
+
+    flo's FM synth is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    flo's FM synth is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with flo's FM synth.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef __READWAVE_H__
 #define __READWAVE_H__
 
